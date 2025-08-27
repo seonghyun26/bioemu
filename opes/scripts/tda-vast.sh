342e448a92dd59c2d7f5b3d0745d3cfca620f296
hyun <hyun26@kaist.ac.kr>
2025-08-27 07:56:39 +0000

    upd - vast script

---
+++ opes/scripts/tda-vast.sh
@@ -0,0 +1,17 @@
+cd ../
+
+GPU=${1:-4}
+sigma=$2
+date=$(date +%m%d_%H%M%S)
+echo $date
+
+python main.py \
+    --config-name tda \
+    step=600_000_000 \
+    date=$date \
+    sigma=$sigma \
+    gpu=$GPU &
+
+python analysis_opes.py \
+        --config-name tda \
+        date=$date
