f6bcd4091031aa92f199b573738d4f747917d1c9
hyun <hyun26@kaist.ac.kr>
2025-09-10 13:08:22 +0000

    upd - ours start gpu id arg

--- opes/scripts/ours.sh
+++ opes/scripts/ours.sh
@@ -2,6 +2,7 @@ cd ../
 
 method=ours
 molecule=${1:-"2jof"}
+start_gpu=${2:-0}
 date=$(date +%m%d_%H%M%S)
 echo $date
 
@@ -9,7 +10,7 @@ python main.py \
     --config-name $method-$molecule \
     date=$date \
     +tags=['svr'] \
-    start_gpu=0
+    start_gpu=$start_gpu
 
 python analysis_opes.py \
     --config-name $method-$molecule \
