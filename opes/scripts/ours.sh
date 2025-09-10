ee4f1f7f0da5f44858e014b255218c2ef750f24d
hyun <hyun26@kaist.ac.kr>
2025-09-10 11:06:25 +0000

    upd - ours script

--- opes/scripts/ours.sh
+++ opes/scripts/ours.sh
@@ -3,14 +3,13 @@ cd ../
 method=ours
 molecule=${1:-"2jof"}
 date=$(date +%m%d_%H%M%S)
-date="0906_161400"
 echo $date
 
-# python main.py \
-#     --config-name $method-$molecule \
-#     date=$date \
-#     +tags=['svr'] \
-#     start_gpu=4 
+python main.py \
+    --config-name $method-$molecule \
+    date=$date \
+    +tags=['svr'] \
+    start_gpu=0
 
 python analysis_opes.py \
     --config-name $method-$molecule \
