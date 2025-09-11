f3494ad9474e5f829f9cb949ffb053a9f664e5b8
hyun <hyun26@kaist.ac.kr>
2025-09-11 11:33:18 +0000

    upd - vast start gpu id

--- opes/scripts/vast-tda.sh
+++ opes/scripts/vast-tda.sh
@@ -2,12 +2,14 @@ cd ../
 
 method=tda
 molecule=${1:-"cln025"}
+start_gpu=${2:-"0"}
 date=$(date +%m%d_%H%M%S)
 echo $date
 
 python main.py \
     --config-name $method-$molecule \
     date=$date \
+    start_gpu=$start_gpu \
     +tags=['vast']
 
 python analysis_opes.py \
