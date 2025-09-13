169eb5804fb7f3264bf39742631b89db9e7c5023
hyun <hyun26@kaist.ac.kr>
2025-09-13 08:20:39 +0000

    upd - 2jof analysis cfg

--- opes/scripts/run_analysis.sh
+++ opes/scripts/run_analysis.sh
@@ -3,11 +3,12 @@ cd ../
 method=${1:-"tda"}
 molecule=${2:-"cln025"}
 date=${3:-$(date +%m%d_%H%M%S)}
+gmx=${4:-"False"}
 echo $date
 
 python analysis_opes.py \
     --config-name $method-$molecule \
     date=$date \
-    ++analysis.gmx=True \
+    analysis.gmx=$gmx \
     +tags=['vast']
 
