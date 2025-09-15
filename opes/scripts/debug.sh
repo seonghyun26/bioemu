af513afda17bd0c1965a92f441001075cd9f7b0a
hyun <hyun26@kaist.ac.kr>
2025-09-15 01:57:12 +0000

    debug - cfgs

--- opes/scripts/debug.sh
+++ opes/scripts/debug.sh
@@ -1,15 +1,19 @@
 cd ../
 
-method="tica"
-ntomp=${2:-"1"}
-start_gpu=${3:-"0"}
+method=${1:-"tica"}
+molecule=${2:-"1fme"}
+ntomp=${3:-"1"}
+start_gpu=${4:-"0"}
+max_seed=${5:-"3"}
 date="debug"
 echo $date
 
 python main.py \
-    --config-name debug \
+    --config-name $method-$molecule \
     date=$date \
     +tags=['debug'] \
+    opes.step=10_000 \
+    opes.max_seed=$max_seed \
     start_gpu=$start_gpu \
     opes.ntomp=$ntomp
 
@@ -19,6 +23,6 @@ python analysis_opes.py \
     --config-name debug \
     date=$date \
     +tags=['debug'] \
-    opes.max_seed=0 \
-    analysis.skip_steps=0 \
-    analysis.unit_steps=1
+    opes.max_seed=$max_seed \
+    analysis.skip_steps=10 \
+    analysis.unit_steps=10
